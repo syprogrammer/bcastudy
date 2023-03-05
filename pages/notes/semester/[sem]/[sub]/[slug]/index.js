7069810b154c636455e2ecc1d19729883e390c57
syprogrammer <105416337+syprogrammer@users.noreply.github.com>
2023-03-05 16:36:02 +0530

    Update index.js

--- pages/notes/semester/[sem]/[sub]/[slug]/index.js
+++ pages/notes/semester/[sem]/[sub]/[slug]/index.js
@@ -63,7 +63,7 @@ export async function getStaticPaths() {
         params: { 
             slug:item.slug.current,
             sem:item.semester,
-            sub:item.subject 
+            sub:item.subject || "default"
                 },
       }))
 
