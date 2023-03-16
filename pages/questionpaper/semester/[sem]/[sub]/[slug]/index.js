ea5d2ceb7e8d6cf48914f2f59a534a04ea3b5cab
syprogrammer <sonaly647@gmail.com>
2023-03-16 17:55:08 +0530

    responsive

--- pages/questionpaper/semester/[sem]/[sub]/[slug]/index.js
+++ pages/questionpaper/semester/[sem]/[sub]/[slug]/index.js
@@ -15,7 +15,7 @@ const RichTextComponent = {
         return null
       }
       return (
-        <div className="relative w-1/2 border h-fit m-10 mx-auto ">
+        <div className="relative md:w-1/2 border h-fit m-10 mx-auto ">
           <img
             className='object-contain'
             alt={value.alt || ' '}
